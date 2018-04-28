3aee2375db197b3e4591c27061932fd59222259b
Joe Jiko <me@joejiko.com>
2018-04-28 14:09:46 -0400

    fix change from section to stacks

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -8,7 +8,7 @@
         });
     </script>
 @stop
-@section('scripts.footer')
+@push('scripts.footer')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
     @if($agent->isDesktop())
@@ -37,7 +37,7 @@
            });
         });
     </script>
-@stop
+@endpush
 @section('styles')
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
